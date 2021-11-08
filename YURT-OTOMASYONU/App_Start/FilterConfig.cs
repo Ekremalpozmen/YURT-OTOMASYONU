efbfa0995577ba20ef87caea40908fbccc56a0a0
Ekrem Alp Özmen <62023846+Ekremalpozmen@users.noreply.github.com>
2021-11-08 22:05:07 +0300

    Add project files.

---
+++ YURT-OTOMASYONU/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace YURT_OTOMASYONU
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
