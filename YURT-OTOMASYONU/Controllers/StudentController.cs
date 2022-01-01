3f7ae589066c16ee328fcb3ac7855359e5e63bb1
Emirhan Limon <ultrarivals@gmail.com>
2022-01-01 15:52:57 +0300

    StudentHomePage tasarımı

---
+++ YURT-OTOMASYONU/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YURT_OTOMASYONU.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
