15de007e53a31386c894f5da0b51ff16845ce8bd
Ekrem Alp Özmen <62023846+Ekremalpozmen@users.noreply.github.com>
2022-01-15 18:22:53 +0300

    Security Service oluşturuldu

---
+++ YURT-OTOMASYONU.Services/Security/SecurityService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YURT_OTOMASYONU.Data;
+using YURT_OTOMASYONU.ViewModels.Security;
+
+namespace YURT_OTOMASYONU.Services.Security
+{
+    public class SecurityService
+    {
+        private readonly YurtOtomasyonEntities _context;
+
+        public SecurityService(YurtOtomasyonEntities context)
+        {
+            _context = context;
+
+        }
+        public Ogrenci OgrenciLogin(LoginViewModel login)
+        {
+            return _context.Ogrenci.FirstOrDefault(x => x.Sifre == login.Password && x.KullaniciAdi == login.Username);
+        }
+    }
+}
--- YURT-OTOMASYONU/Controllers/LoginController.cs
+++ YURT-OTOMASYONU/Controllers/LoginController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using YURT_OTOMASYONU.Services.Security;
 using YURT_OTOMASYONU.ViewModels.Security;
 
 namespace YURT_OTOMASYONU.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly SecurityService _securityService;
+        public LoginController(SecurityService securityService)
+        {
+            _securityService = securityService;
+        }
         // GET: Login
         public ActionResult Index()
         {
@@ -17,21 +24,21 @@ namespace YURT_OTOMASYONU.Controllers
         [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    var loginResult = _securityService.Login(model);
-            //    if (loginResult != null)
-            //    {
-            //        FormsAuthentication.SetAuthCookie(loginResult.Id.ToString(), model.RememberMe);
+            if (ModelState.IsValid)
+            {
+                var loginResult = _securityService.Login(model);
+                if (loginResult != null)
+                {
+                    FormsAuthentication.SetAuthCookie(loginResult.Id.ToString(), model.RememberMe);
 
-            //        return RedirectToAction("Index", "Dashboard");
-            //    }
-            //    else
-            //    {
-            //        ViewBag.kullanıcıyokmesaj = "Geçersiz Kullanıcı Adı veya Şifre";
-            //        return View();
-            //    }
-            //}
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    ViewBag.kullanıcıyokmesaj = "Geçersiz Kullanıcı Adı veya Şifre";
+                    return View();
+                }
+            }
             return View();
         }
     }
